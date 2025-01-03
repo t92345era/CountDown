20a467b505c3dcdb4e9975ea6d569f0a6980f91d
tera <tera0321@gmail.com>
2025-01-03 12:07:02 +0900

    fix: base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'CountDown' : './',
+  base: process.env.GITHUB_PAGES ? './' : './',
   plugins: [react()],
 })
